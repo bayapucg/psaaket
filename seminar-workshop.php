f4e8a418e97a619170bcdb575fbe3f5e54a3aeb8
bayapucg <bayapu.edu004@gmail.com>
2019-01-17 17:27:02 +0530

    comit
    code

---
+++ seminar-workshop.php
@@ -0,0 +1,117 @@
+<?php include("header.php"); ?>
+           
+      <div class="header-space"></div>
+      <!-- Page Header Start Here -->
+      <section class="page-header-bg">
+      <div class="page-header">
+	 
+	  
+
+          <div class="product-header mobile_only_text_center">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-9">
+                    <h2 class="p-t-5">Register</h2>
+                  </div>
+                  <div class="col-md-3 m-t-10">
+                    <a href="#">Home</a> <i class="fa fa-angle-double-right"></i>
+                    <a class="current_page">Seminar/Workshop</a>
+                  </div>
+                </div>
+              </div>  
+          </div> 
+		   </div>
+       
+        </section>
+      <!-- Page Header End Here -->
+
+      <!-- Main Section Start Here -->
+      <div id="main">
+        <!-- About Us Section Start Here-->
+        <section id="about-us" class="about-section">
+          <div class="section-padding">
+            <div class="container">
+              <div class="row about-list">
+                <div class="form col-md-6 contact-form wow fadeInRight col-md-offset-3" data-wow-duration="1.5s" data-wow-offset="10">
+                  <header class="page-header section-header">
+				  <div  id="success-alert">
+								
+							</div>
+					<h2>Seminar/Workshop Register</h2>
+					 
+				  </header>
+				 <form id="#" class="form-horizontal" action="#" method="post">
+
+                   
+				   <div class="form-group">
+                      <div class="col-sm-12">
+					   <label for="sel1">Title</label>
+					  <input class="form-control" type="text" name="" placeholder="Ex: Mr./Ms./Mrs " value="" / required>
+					 </div>
+                    </div>
+					<div class="form-group">
+                      <div class="col-sm-12">
+					  	   <label for="sel1">Name:</label>
+					  <input class="form-control" type="text" name="" placeholder="Enter name" value="" / required>
+					 </div>
+                    </div>
+					<div class="form-group">
+                      <div class="col-sm-12">
+					   <label for="sel1">Address for Correspondence:</label>
+					  <textarea class="form-control" type="text" name="" placeholder="Enter Address for Correspondence:" value="" / required></textarea>
+					 </div>
+                    </div>
+					<div class="form-group">
+                      <div class="col-sm-12">
+					  <label for="sel1">Mobile No:</label>
+					  <input class="form-control" type="text" name="" placeholder="Enter Mobile No" value="" / required>
+					 </div>
+                    </div>
+                   
+                    <div class="form-group">
+                      <div class="col-sm-12 ">
+					  <label for="sel1">Email Id: </label>
+					  <input class="form-control" type="mail" name="" placeholder="Enter Email" value="" /required>
+				     <span style="color:red;">	</span>
+				   </div>
+                    </div>
+					<div class="form-group">
+                      <div class="col-sm-12">
+					   <label for="sel1">College/Institution Name and Address:</label>
+					  <textarea class="form-control" type="text" name="" placeholder="Enter College/Institution Name and Address" value="" / required></textarea>
+					 </div>
+                    </div>
+					  <div class="form-group">
+                      <div class="col-sm-12 ">
+					  <label for="sel1">Amount INR: </label>
+					  <input class="form-control" type="text" name="" value="₹ 5000"/ disabled>
+				     <span style="color:red;">	</span>
+				   </div>
+                    </div>
+					
+                    <div class="form-group">
+                      <div class="col-sm-11">
+					   <input type="submit"  class="btn-theme btn" value="Register  "  />
+                    </div>
+                    </div>
+                  </form>
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- About Us Section End Here-->
+
+      
+       
+		
+		
+
+       
+      </div>
+      <!-- Main Section End Here -->
+
+     
+  <?php include("footer.php"); ?>
+          
